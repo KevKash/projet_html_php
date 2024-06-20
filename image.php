008bd4335b5962bf50d80e74eba363bf76e65a43
kevin <kashamakevin270@gmail.com>
2024-06-20 16:51:06 +0200

    projet html php
    20/06/2024

--- image.php
+++ image.php
@@ -1,2 +1,27 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
+<mage>
+ <section class="fond"><h1>Images</h1></section>
+ <sectionim><p>Il arrive souvent que l'idée illustrée se confonde avec l'idée expliquée, une preuve pouvant servir à expliquer quelque chose, tout comme une explication pouvant aussi illustrer. Ces deux parties peuvent donc être traitées séparément ou ensemble. L'ordre de présentation de ces trois parties peut varier l'explication peut précéder ou suivre la preuve, selon la façon avec laquelle on présente les illustrations.
+
+ 
+
+Comme pour le plan, on trouve trois types de paragraphes dans la dissertation critique (on peut obtenir des paragraphes hybrides qui mêlent ces trois types) :
+
+Le paragraphe démonstratif présentera d'abord l'idée énoncée, ensuite les parties illustratives (idée illustrée) et les parties explicatives (idée expliquée), puis enfin un rappel de l'idée énoncée avec une transition.
+
+Le paragraphe dialectique avance d'abord une idée défendant un point de vue (thèse) avec illustration et explication, pour ensuite l'opposer à l'idée contraire (antithèse) avec illustration et explication pour finalement conclure et donner la véritable idée énoncée du paragraphe (synthèse). Ce type de paragraphe est plus complexe et exige une plus grande habileté d'écriture, à défaut de laquelle il peut facilement donner l'impression que l'élève se contredit.
+
+Le paragraphe comparatif fonctionne comme le paragraphe démonstratif, sauf qu'à l'intérieur du même paragraphe, on établit la démonstration entre deux éléments que l'on veut comparer. L'exemple qui suit montre ce type de paragraphe.</p>
+ </sectionim>
+ 
+ <sectionmage>
+    <im>Image de fon CSS</im>
+    <div class="tour"><img src="./public/assets/img/toureiffel.jpg" alt="" id="tour"></div>
+    <im>Image de fon HTML</im>
+    <div class="double">
+        <img src="./public/assets/img/image.jpg" alt="" id="pont"> 
+        <img src="./public/assets/img/image.jpg" alt="" id="pont">
+    </div>
+ </sectionmage>
+</mage>
 <?php include __DIR__ . "/public/footer.html.php";?>
--- selecteur.php
+++ selecteur.php
@@ -1,19 +1,23 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
 
 
-<selec>
-    <title>Selecteur</title>
+<cteur>
+    <h1>Selecteur</h1>
     
+  <h2>les Selecteur</h2>
+
     <div class="pink">
                     text n°1</div>
 
     <div class="red">
-                     text n°2
-                     text n°3
-                     text n°4
-                     </div>
+                     text n°2</div>
+    <div class="red">
+                     text n°3</div>
+    <div class="red">                 
+                     text n°4</div>
+                     
 
-    <div class="black">
+    <div>
                      text n°5
                      text n°6
                      </div>
@@ -28,10 +32,10 @@
     <div class="orange">
                      ZONE A
                      </div>
-    <div class="b">ZONE B ZONE B <div></div> Zone B zONE B</div>
+    <div class="b"> ZONE B ZONE B<div class="bleu">TEXTE</div> Zone B zONE B</div>
     <div class="c">ZONE C </div>
-    <div class="d">ZONE D</div>
-</selec>
+    <div class="d">ZONE <e>D</e></div>
+</cteur>
 
 
 <?php include __DIR__ . "/public/footer.html.php";?>
