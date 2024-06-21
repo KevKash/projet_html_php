8ef49b6feea9aa69abd3b45e10e920ad7b774034
kevin <kashamakevin270@gmail.com>
2024-06-21 16:50:33 +0200

    21/06/2024
    php_html

--- position.php
+++ position.php
@@ -1,11 +1,41 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
 <main>
+<div class="firstmain">
     <h2>Position</h2>
     <p>centrez un element de type block</p>
     <section id="position">
         <div class="square1"></div>
     </section>
-    <p>position absolue tema la nav</p>
+    <h3>position absolue tema la nav</h3>
     <p>Carré JAUNE!Regarde en haut!</p>
+</div>
+<div class="secmain">
+    <h3>Position relative</h3>
+    <section id="relative">
+        <div class="lilsquare"></div>
+    </section>
+    
+</div>
+<div class="thirdmain">
+    <h3>Position fixed </h3>
+    <p>Bouton retour vers le haut <p>
+
+    <p>Le pourcentage </p>
+  
+   <div class="bois">30%</div>
+   <div class="rose">50%</div>
+   <div class="beige">100%</div>
+ 
+
+</div>
+<div class="flex">
+    <p>Flexbox</p>
+    <div id="flex">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+    </div>
+</div>
 </main>
 <?php include __DIR__."/public/footer.html.php"; ?>
--- zoning.php
+++ zoning.php
@@ -1,2 +1,23 @@
 <?php include __DIR__ . "/public/header.html.php"; ?>
+<main>
+  
+    <ction class="zoninghaut">
+        <div id="zobleu"></div>
+        <div id="zobleu"></div>
+        <div id="zobleu"></div>
+    </ction>
+
+
+    <ction>menu</ction>
+
+
+    <ction class="zoningbas">
+        <div></div>
+        <div></div>
+    </ction>
+
+
+    <ction>bas</ction>
+
+</main>
 <?php include __DIR__ . "/public/footer.html.php";?>
